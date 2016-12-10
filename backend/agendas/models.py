4f5a6eaed71d5682253f1ec435f7e3ed03f8c9b2
Alison <github@bankofknowledge.net>
2016-12-10 11:09:24 -0600

    add models and database

--- backend/agendas/models.py
+++ backend/agendas/models.py
@@ -2,4 +2,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class SchoolDistrict(models.Model):
+	name = models.CharField(max_length=255)
+	url_to_scrape = models.TextField()
+	grade_levels = models.CharField(max_length=50, null=True)
+	mailing_city = models.CharField(max_length=60)
+	mailing_state = models.CharField(max_length=2)
+	district_number = models.CharField(max_length=4, null=True)
+	can_parse = models.BooleanField(default=False)
+	ever_matched_keywords = models.BooleanField(default=False)
+	created_at = models.DateTimeField('Created At',auto_add_now=True)
+	modified_at = models.DateTimeField('Modified At',auto_now=True)
+
+class Agendas(models.Model):
+	school_district = models.ForeignKey(SchoolDistrict)
+	meeting_at = models.DateTimeField()
+	link_to_agenda = models.TextField()
+	keyword_flag = models.BooleanField()
+	keywords = models.TextField()
+	created_at = models.DateTimeField(auto_add_now=True)
+	modified_at = models.DateTimeField(auto_now=True)
+
+
+
+
