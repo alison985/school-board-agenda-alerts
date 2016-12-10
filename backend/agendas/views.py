52394660b27442b3436449abb884c143aa2649c6
Alison <github@bankofknowledge.net>
2016-12-10 13:37:04 -0600

    commit working keyword_flag filter

--- backend/agendas/views.py
+++ backend/agendas/views.py
@@ -13,6 +13,17 @@ class AgendasList(generics.ListCreateAPIView):
 	model = Agendas 
 	permission_class = (AllowAny,)
 	order_by = ('meeting_at')
+	
+def get_queryset(self,request):
+	keyword_flag = request.query_params.get('keyword_flag',"True")
+	if keyword_flag == "All":
+		return Agendas.objects.filter(meeting_at__gte=datetime(now.year,now.month,now.day))
+	elif keyword_flag == "True":
+		return Agendas.objects.filter(meeting_at__gte=datetime(now.year,now.month,now.day),keyword_flag=True)
+	elif keyword_flag == "False":
+		return Agendas.objects.filter(meeting_at__gte=datetime(now.year,now.month,now.day),keyword_flag=False)
+
+
 
 class AgendasDetail(generics.RetrieveAPIView):
 	queryset = Agendas.objects.all()
