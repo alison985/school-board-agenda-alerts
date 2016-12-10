969b7c14df2040e70e7fa3307b727b381c7d7f9c
Alison <github@bankofknowledge.net>
2016-12-10 15:31:40 -0600

    add school district info to api calls

--- backend/schoolboardagendas/serializers.py
+++ backend/schoolboardagendas/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers 
-from schoolboardagendas.models import SchoolBoardAgendas
+from schoolboardagendas.models import *
 
+
+class SchoolDistrictSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = SchoolDistrict
+		fields = ('name','mailing_city','mailing_state')
 
 class SchoolBoardAgendasSerializer(serializers.ModelSerializer):
+	school_district = SchoolDistrictSerializer(read_only=True)
 	class Meta:
 		model = SchoolBoardAgendas 
-		fields = '__all__'
+		fields = ('school_district','meeting_at','link_to_agenda','keyword_flag','keywords')
+
